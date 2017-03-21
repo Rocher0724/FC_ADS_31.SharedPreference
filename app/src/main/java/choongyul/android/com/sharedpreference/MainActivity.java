3b1ced5c54e67b5d1c9d1cdc47dfdcd8fd038404
ROCHER0724\NT <rocher0724.dev@gmail.com>
2017-03-21 12:06:56 +0900

    Shared preference로 설정값을 불러왔습니다.

--- app/src/main/java/choongyul/android/com/sharedpreference/MainActivity.java
+++ app/src/main/java/choongyul/android/com/sharedpreference/MainActivity.java
@@ -1,11 +1,14 @@
 package choongyul.android.com.sharedpreference;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -19,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
     PropertyUtil propertyUtil;
 
-    // 내부저장소 절대경로 가져오기 , files 경로를 포함함. (/data/data/패키지명/files)
-    final String propertyFile = "test.properties";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         if("false".equals(propertyUtil.getProperty("firstOpen"))) {
             layout2.setVisibility(View.GONE);
         }
+
+        //세팅된 값을 가져와서 화면에 뿌린다.
+        loadSetting();
     }
 
     public void closeHelp(View view) {
@@ -44,6 +48,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void saveSetting(View view) {
 
+        // 1. Preference 생성하기
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        // 앱전체에서 사용하기 위해서는 아래와같이 쓴다.
+//        SharedPreferences sharedPr1ef = getSharedPreferences("file name",Context.MODE_PRIVATE);
+
+        // 2. Shared Preference의 값을 입력하기 위해서는 에디터를 통해서만 가능하다.
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // 에디터에 키, 값 순서로 입력한다.
+        editor.putString("email" , etEmail.getText().toString() );
+        editor.putBoolean("shuffle" , aSwitch.isChecked() );
+
+        editor.commit();
+
+        Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
+    }
+    public void loadSetting() {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String email = sharedPref.getString("email", null );
+                                            // 키 , 값이없을때 디폴트로 집어넣을것
+        boolean shuffle = sharedPref.getBoolean("shuffle", false );
+        etEmail.setText(email);
+        aSwitch.setChecked(shuffle);
     }
 
 
--- app/src/main/java/choongyul/android/com/sharedpreference/PropertyUtil.java
+++ app/src/main/java/choongyul/android/com/sharedpreference/PropertyUtil.java
@@ -18,20 +18,20 @@ public class PropertyUtil {
     private static Context context;
 
     // 생성자가 호출될때 internalStorage를 세팅해줘야 한다.
+    // 내부저장소 절대경로 가져오기 , files 경로를 포함함. (/data/data/패키지명/files)
     private PropertyUtil() {
         internalStoragePath = context.getFilesDir().getAbsolutePath();
     }
 
     public static PropertyUtil getInstance (Context ctx) {
+        context = ctx;
         if(instance == null) {
             instance = new PropertyUtil();
         }
-        context = ctx;
         return instance;
     }
     public String getProperty(String key) {
         String value = "";
-
         Properties prop = new Properties();
         try{
             FileInputStream fis = new FileInputStream(internalStoragePath + "/" + PROP_FILE);
@@ -40,29 +40,25 @@ public class PropertyUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        prop.list(System.out); // 프로퍼티 목록 전체 나열하기
-
+//        prop.list(System.out); // 프로퍼티 목록 전체 나열하기
         value = prop.getProperty(key);
-
         return value;
     }
 
-    public void saveProperty(String key , String value) {
+    public void saveProperty(String key, String value){
         Properties prop = new Properties();
         prop.put(key, value);
-
         try {
             // 앱의 내부저장소/files/test.properties 파일을 저장
-            FileOutputStream fos = new FileOutputStream(internalStoragePath + "/test.properties");
-            prop.store(fos, "comment");
-
-            // 저장후 파일을 닫아준다.
+            FileOutputStream fos = new FileOutputStream(internalStoragePath + "/" + PROP_FILE);
+            prop.store(fos, "comment"); // key=value
+            // 저장 후 파일을 닫아준다
             fos.close();
-        }catch (Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
+
 
 
 
